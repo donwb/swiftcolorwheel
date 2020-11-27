9fcc99daf3f24d68505c3da2db654a1ac92116dd
don browning <don.browning@gmail.com>
2020-11-26 22:34:30 -0500

    Initial Commit

---
+++ colorwheel/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  colorwheel
+//
+//  Created by Don Browning on 11/26/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
