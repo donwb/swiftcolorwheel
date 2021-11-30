7afef099e5d09af4e544845c00d62ec76a0ec264
Don Browning <don.browning@gmail.com>
2021-11-30 13:41:50 -0500

    start some base class refactoring

---
+++ colorwheel/LightControl.swift
@@ -0,0 +1,20 @@
+//
+//  LightControl.swift
+//  colorwheel
+//
+//  Created by Don Browning on 11/30/21.
+//
+
+import Foundation
+
+class LightControl {
+    private let _hueUsername = "PNNmIH9ajNZy2p1nhVnzsEtwYgsEmY2zvBjrrhlq"
+    var HueUsername: String {
+        get {
+            return _hueUsername
+        }
+    }
+    
+    // need to pull getLightInfo and getLightDetail, etc from ViewController into this class
+    
+}
